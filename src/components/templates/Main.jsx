50ed9d9bc9b18cbae4a9b7bacd8fc159182a1e58
Arthur Guedes <arthur.guedes@ccc.ufcg.edu.br>
2020-06-22 20:11:07 -0300

    Import and add card component to main template

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 
-import './Main.css';
+import './Main.css'
+import Card from '../card/Card'
 
 export default () =>
-    <div className="main">
-        {/* TODO: Extrair esse bloco para um componente */}
-        <div className="form card">
-            <h2>Agendamento para Celebrações</h2>
-            <p>é de fundamental importância que os dados fornecidos estejam corretos, para que possamos gerenciar de maneira correta e atentendo as exigências dos órgãos competentes.</p>
-        </div>
-        {/* TODO: Extrair esse bloco para um componente */}
-        <div className="info card">
-            <h2>Cuide de Você e da comunidade</h2>
-            <p>Evite Aglomerações antes e depois das Missas;</p>
-            <p>Utilize a máscara em todo o percurso até a igreja, como também durante e após a celebração;</p>
-            <p>Lave bem as mãos e use sempre álcool em gel A70;</p>
-            <p>Respeite o distanciamento social;</p>
-        </div>
-    </div>
+    <main className="main">
+        <Card title="Agendamento para Celebrações">
+            <p>
+                É de fundamental importância que os dados fornecidos estejam corretos, 
+                para que possamos gerenciar de maneira correta e atentendo as exigências dos órgãos competentes.
+            </p>
+        </Card>
+        <Card title="Cuide de Você e da comunidade" backgroundColor="blue">
+            <div>
+                <p>Evite Aglomerações antes e depois das Missas;</p>
+                <p>Utilize a máscara em todo o percurso até a igreja, como também durante e após a celebração;</p>
+                <p>Lave bem as mãos e use sempre álcool em gel A70;</p>
+                <p>Respeite o distanciamento social;</p>
+            </div>
+        </Card>
+    </main>
